89e928c1fdf9775398a985fb1f3d5fcd275a4bc3
renan028 <renan028@gmail.com>
2020-09-29 09:42:12 -0300

    fix identation

--- include/queries/change_parameter.hpp
+++ include/queries/change_parameter.hpp
@@ -23,23 +23,24 @@
 #include "json.hpp"
 #include "query.hpp"
 
-namespace zmqserver{
+namespace zmqserver
+{
 
 template <class T>
 class ChangeParameter : public Query
 {
-    protected:
-        T _values;
-    public:
-        ChangeParameter(int id, T values) : 
-            Query(id),
-            _values(values)
-        {
-            _query["type"] = "change-parameter";
-        }
-        ~ChangeParameter(){};
-        
-        void setParameter(const T& values){_values = values;};
+protected:
+  T _values;
+
+public:
+  ChangeParameter(int id, T values) : Query(id),
+                                      _values(values)
+  {
+    _query["type"] = "change-parameter";
+  }
+  ~ChangeParameter(){};
+
+  void setParameter(const T &values) { _values = values; };
 };
 
-}
+} // namespace zmqserver
--- include/queries/change_velocity.hpp
+++ include/queries/change_velocity.hpp
@@ -23,45 +23,44 @@
 #include "json.hpp"
 #include "change_parameter.hpp"
 
-namespace zmqserver{
+namespace zmqserver
+{
 
-struct Velocity{
-    std::unordered_map<std::string, double> values;
+struct Velocity
+{
+  std::unordered_map<std::string, double> values;
 
-    Velocity(double vmin, double vmax)
-    {
-        values.insert(std::make_pair("1", vmin));
-        values.insert(std::make_pair("2", vmax));
-    }
-    Velocity()
-    {
-        values.insert(std::make_pair("1", 0.0));
-        values.insert(std::make_pair("2", 0.0));
-    }
+  Velocity(double vmin, double vmax) {
+    values.insert(std::make_pair("1", vmin));
+    values.insert(std::make_pair("2", vmax));
+  }
+  Velocity() {
+    values.insert(std::make_pair("1", 0.0));
+    values.insert(std::make_pair("2", 0.0));
+  }
 };
-
 
 class ChangeVelocity : public ChangeParameter<Velocity>
 {
-    public:
-        ChangeVelocity(int id, Velocity values) : 
-            ChangeParameter<Velocity>(id, values)
-        {
-            _query["ParameterSetRepository"] = "CommNavigationObjects";
-            _query["ParameterSet"] = "CdlParameter";
-            _query["Component"] = "SmartCdlServer";
-            _query["Parameter"] = "transvel";
-        }
-        ~ChangeVelocity(){};
-        
-        std::string dump() override{
-            nlohmann::json values;
-            
-            values["1"] = _values.values["1"];
-            values["2"] = _values.values["2"];
-            _query["values"] = values;
-            return Query::dump();
-        }
+public:
+  ChangeVelocity(int id, Velocity values) : ChangeParameter<Velocity>(id, values)
+  {
+    _query["ParameterSetRepository"] = "CommNavigationObjects";
+    _query["ParameterSet"] = "CdlParameter";
+    _query["Component"] = "SmartCdlServer";
+    _query["Parameter"] = "transvel";
+  }
+  ~ChangeVelocity(){};
+
+  std::string dump() override
+  {
+    nlohmann::json values;
+
+    values["1"] = _values.values["1"];
+    values["2"] = _values.values["2"];
+    _query["values"] = values;
+    return Query::dump();
+  }
 };
 
-}
+} // namespace zmqserver
